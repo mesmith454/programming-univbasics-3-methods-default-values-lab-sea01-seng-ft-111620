4629c9b467a39b4379fbad6491ffe87fec6e3c94
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-26 20:26:23 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,8 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+veg1 = "carrots"
+veg2 = "potatoes"
+
+def meal_choice(veg1, veg2, protein = "tofu")
+  puts "What a nutritious meal!"
+  "A plate of #{protein} with #{veg1} and #{veg2}."
